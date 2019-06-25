822429c6c6e1970c0e80f2d9ea6a7cd0927ddc64
Arthur Tolentino <Arthur@Arthurs-MBP.fios-router.home>
2019-06-25 10:14:33 -0400

    implement process strategy

--- src/main/java/com/example/demo/FunctionalJavaAsyncProcessingApplication.java
+++ src/main/java/com/example/demo/FunctionalJavaAsyncProcessingApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.components.JavaConcurrencyStrategy;
+import com.example.demo.components.ProcessStrategy;
 
 @SpringBootApplication
 public class FunctionalJavaAsyncProcessingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FunctionalJavaAsyncProcessingApplication.class, args);
+	}
+	
+	@Bean
+	public ProcessStrategy processStrategy(@Value("${logprocessor.thread.count}") int threads) {
+		return new JavaConcurrencyStrategy(threads);
 	}
 
 }
